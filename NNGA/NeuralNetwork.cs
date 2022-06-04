38f62c8e7364ba8946439abfd8274ed14442a612
Danyy427 <ozgur.guzeldereli@gmail.com>
2022-06-04 23:06:55 +0300

    Added NN

--- NNGA/NeuralNetwork.cs
+++ NNGA/NeuralNetwork.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace NNGA
 {
+    internal class NodeData
+    {
+        public int index { get; set; }
+        public NodeType type { get; set; }
+    }
+
+    internal class ConnectionData
+    {
+        public int sourceIndex { get; set; }
+        public NodeType sourceType { get; set; }
+
+        public int destinationIndex { get; set; }
+        public NodeType destinationType { get; set; }
+    }
+
     internal class NeuralNetwork
     {
         private List<Node> _inputNodes;
@@ -95,19 +110,402 @@ namespace NNGA
 
         public void MakeConnectionByNodeRefRandom(ref Node from, ref Node to, int range = 4)
         {
+            if((!_internalNodes.Contains(from) || !_inputNodes.Contains(from)) && 
+                (!_internalNodes.Contains(to) || !_outputNodes.Contains(to)))
+            {
+                throw new Exception("Invalid Connection. The nodes are not a part of the neural network");
+            }
             from.Connect(to, true, range);
         }
 
         public void MakeConnectionByNodeRef(ref Node from, ref Node to, double weight)
         {
+            if ((!_internalNodes.Contains(from) || !_inputNodes.Contains(from)) &&
+                (!_internalNodes.Contains(to) || !_outputNodes.Contains(to)))
+            {
+                throw new Exception("Invalid Connection. The nodes are not a part of the neural network");
+            }
             from.Connect(to, weight);
         }
 
         public void MakeConnectionByNodeRef(ref Node from, ref Node to, bool isRandom = true, int range = 4)
         {
+            if ((!_internalNodes.Contains(from) || !_inputNodes.Contains(from)) &&
+                (!_internalNodes.Contains(to) || !_outputNodes.Contains(to)))
+            {
+                throw new Exception("Invalid Connection. The nodes are not a part of the neural network");
+            }
             from.Connect(to, isRandom, range);
         }
 
+        public void MakeConnection(NodeType sourceType, int sourceIndex, NodeType destType, int destination, double weight)
+        {
+            if(sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], weight);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], weight);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], weight);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], weight);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnection(NodeType sourceType, int sourceIndex, NodeType destType, int destination, bool isRandom = true, int range = 4)
+        {
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], isRandom, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], isRandom, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], isRandom, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], isRandom, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionRandom(NodeType sourceType, int sourceIndex, NodeType destType, int destination, int range)
+        {
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], true, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], true, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], true, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], true, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionByNodeData(NodeData node1, NodeData node2, double weight)
+        {
+            var sourceType = node1.type;
+            var destType = node2.type;
+            var sourceIndex = node1.index;
+            var destination = node2.index;
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], weight);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], weight);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], weight);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], weight);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionByNodeData(NodeData node1, NodeData node2, bool isRandom = true, int range = 4)
+        {
+            var sourceType = node1.type;
+            var destType = node2.type;
+            var sourceIndex = node1.index;
+            var destination = node2.index;
+
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], isRandom, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], isRandom, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], isRandom, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], isRandom, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionByNodeData(NodeData node1, NodeData node2, int range)
+        {
+            var sourceType = node1.type;
+            var destType = node2.type;
+            var sourceIndex = node1.index;
+            var destination = node2.index;
+
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], true, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], true, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], true, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], true, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionByConnectionData(ConnectionData connectionData, double weight)
+        {
+            var sourceType = connectionData.sourceType;
+            var destType = connectionData.destinationType;
+            var sourceIndex = connectionData.sourceIndex;
+            var destination = connectionData.destinationIndex;
+
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], weight);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], weight);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], weight);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], weight);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionByConnectionData(ConnectionData connectionData, bool isRandom = true, int range = 4)
+        {
+            var sourceType = connectionData.sourceType;
+            var destType = connectionData.destinationType;
+            var sourceIndex = connectionData.sourceIndex;
+            var destination = connectionData.destinationIndex;
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], isRandom, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], isRandom, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], isRandom, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], isRandom, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
+
+        public void MakeConnectionByConnectionData(ConnectionData connectionData, int range)
+        {
+            var sourceType = connectionData.sourceType;
+            var destType = connectionData.destinationType;
+            var sourceIndex = connectionData.sourceIndex;
+            var destination = connectionData.destinationIndex;
+            if (sourceType == NodeType.InputNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_internalNodes[destination], true, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _inputNodes[sourceIndex].Connect(_outputNodes[destination], true, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else if (sourceType == NodeType.InternalNode)
+            {
+                if (destType == NodeType.InternalNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_internalNodes[destination], true, range);
+                }
+                else if (destType == NodeType.OutputNode)
+                {
+                    _internalNodes[sourceIndex].Connect(_outputNodes[destination], true, range);
+                }
+                else
+                {
+                    throw new Exception("The destination cannot be an input node");
+                }
+            }
+            else
+            {
+                throw new Exception("The source cannot be an output node");
+            }
+        }
     }
 }
     
